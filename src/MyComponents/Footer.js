f2653226091824433b34ddbc4639a35a3df6981f
anshika2306 <anshika.mahajan10@gmail.com>
2022-11-01 14:22:20 +0530

    Pushing to origin

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle = {
+  //     position: "relative",
+  //     top: "70vh",
+  //     width: "100%"
+  // }
+  
+  return (
+    <footer className="bg-dark text-light">
+      <p className="text-center my-3 py-1">
+      Copyright &copy; MyTodolist.com
+      </p>
+       
+    </footer>
+  )
+}
